2eea1ff2bfc4d3d21910cc61fec20d0bd9fb6de5
Justin Ramirez <justinjlramirez@gmail.com>
2017-03-14 06:33:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  var rankList=document.querySelectorAll('ul.ranked-list li');
+  for (var i=0; i<rankList.length;i++) {
+    rankList[i].parseInt() = (rankList[i])+n;
+  }
+  return rankList;
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div')
+}
